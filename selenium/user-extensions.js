cbb81c16e126538fcda00d4c2c73c5a75e647a86
Travis Jenkins <travis_jenkins@abercrombie.com>
2016-02-01 13:46:07 -0500

    Updating to store the emails entered into an array. Doing it in an array so that if you enter several emails you can get all the ones entered later.

--- selenium/user-extensions.js
+++ selenium/user-extensions.js
@@ -1,61 +1,93 @@
+var application = {
+        "session" : {
+            "emailsEntered" : []
+        },
+        "generateRandom": {
+            "alphaNumeric" : function (settings) {
+                //Taken from: http://stackoverflow.com/questions/1349404/generate-a-string-of-5-random-characters-in-javascript
+                var i = 0,
+                    maxLength = application.generateRandom.integer(),
+                    possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
+                    randomSettings = {},
+                    text = "";
+                    
+                randomSettings = {
+                    "min": 2,
+                    "max": possible.length
+                }
+
+                if (settings && settings.maxLength) {
+                    maxLength = settings.maxLength;
+                }
+
+                for (i=0; i < maxLength; i += 1) {
+                    text += possible.charAt(application.generateRandom.integer(randomSettings));
+                }
+
+                return text;
+            },
+            "integer" : function (settings) {
+                //Taken from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+                var response = 0;
+
+                if (typeof settings === "undefined") {
+                    settings = {}
+                }
+
+                if (typeof settings.min === "undefined") {
+                    settings.min = 1;
+                }
+
+                if (typeof settings.max === "undefined") {
+                    settings.max = 15;
+                }
+
+                response = Math.random() * (settings.max - settings.min) + settings.min;
+
+                return Math.floor(response);
+            },
+            "topLevelDomain" : function () {
+                var i = 0,
+                    possible = ["com", "net", "gov", "org", "mil", "co.uk"],
+                    randomSettings = {},
+                    text = "";
+                    
+                randomSettings = {
+                    "max": possible.length
+                }
+
+                text = possible[application.generateRandom.integer(randomSettings)];
+
+                return text;
+            }
+        }
+    };
+
 Selenium.prototype.doTypeRandomEmail = function (locator) {
-    var data = "",
-        element = this.page().findElement(locator),
-        getRandomInt = function(settings) {
-            //Taken from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
-            var response = 0;
+    var address = "",
+        data = "",
+        domain = "",
+        element = this.page().findElement(locator);
 
-            if (typeof settings === "undefined") {
-                settings = {}
-            }
+    if (data === "" && element !== null) {
+        address = application.generateRandom.alphaNumeric({
+            "maxLength": 10
+        });
 
-            if (typeof settings.min === "undefined") {
-                settings.min = 1;
-            }
+        domain = application.generateRandom.alphaNumeric({
+            "maxLength": 10
+        });
 
-            if (typeof settings.max === "undefined") {
-                settings.max = 15;
-            }
-
-            response = Math.random() * (settings.max - settings.min) + settings.min;
-
-            return Math.floor(response);
-        },
-        randomAlphaNumeric = function () {
-            //Taken from: http://stackoverflow.com/questions/1349404/generate-a-string-of-5-random-characters-in-javascript
-            var i = 0,
-                possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789",
-                randomSettings = {},
-                text = "";
-                
-            randomSettings = {
-                "max": possible.length
-            }
-
-            for (i=0; i < getRandomInt(); i += 1) {
-                text += possible.charAt(getRandomInt(randomSettings));
-            }
-
-            return text;
-        },
-        randomTopLevelDomain = function () {
-            var i = 0,
-                possible = ["com", "net", "gov", "org", "mil", "co.uk"],
-                randomSettings = {},
-                text = "";
-                
-            randomSettings = {
-                "max": possible.length
-            }
-
-            text = possible[getRandomInt(randomSettings)];
-
-            return text;
-        };
-
-    if (true) {
-        data = randomAlphaNumeric() + "@" + randomAlphaNumeric() + "." + randomTopLevelDomain();
+        data = address + "@" + domain + "." + application.generateRandom.topLevelDomain();
     }
 
     this.browserbot.replaceText(element, data);
+    application.session.emailsEntered.push(data);
+};
+
+Selenium.prototype.doGetRandomEmail = function (locator) {
+    var data = "",
+        element = this.page().findElement(locator);
+
+    LOG.info("blubber = " + application.session.emailsEntered);
 };
